b24b658483619ef21ae3b8db32574911c1d4b07b
TheTarnished42 <apuroop42@gmail.com>
2023-11-14 02:05:40 +0530

    Categories Added

--- bin/add_book.c
+++ bin/add_book.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 // Declaring the book structure.
 typedef struct book
 {
@@ -13,18 +14,165 @@ typedef struct book
     int available_UID[50];
     int issued_UID[50];
 } BOOK;
+
+typedef struct category
+{
+    int no;
+    char name[20];
+} CAT;
+
 void add_categories()
-{ // Work pending
-    printf("Add categories has been called\n");
-    char cat[50];
+{
+    FILE *catptr;
+    CAT c;
+    c.no = 0;
+    catptr = fopen("../BooksDB/Categories.txt", "r+");
+    while (fread(&c, sizeof(c), 1, catptr))
+    {
+    }
+    c.no++;
+    int flag = 0;
+    printf("\nADD NEW CATEGORIES: Enter 0 to QUIT\n\n");
+    while (flag == 0)
+    {
+        int flag2;
+        while (1)
+        {
+            printf("NEW Category Name [%d]: ", (c.no));
+            fgets(c.name, 20, stdin);
+            c.name[strcspn(c.name, "\n")] = '\0';
+            if (c.name[0] == '0')
+            {
+                flag++;
+                break;
+            }
+            while (1)
+            {
+                printf("Confirm New Category[1] or Enter Again[2]: ");
+                scanf("%d", &flag2);
+                getchar();
+                if (flag2 == 1)
+                {
+                    fwrite(&c, sizeof(c), 1, catptr);
+                    c.no++;
+                    printf("Category ADDED.\n\n");
+                    break;
+                }
+                else if (flag2 == 2)
+                    printf("Enter Again.\n");
+                else
+                    printf("Invalid input.\n");
+            }
+        }
+        printf("Exited from ADD NEW CATEGORIES.\n");
+    }
+    fclose(catptr);
 }
+
+void input_categories(BOOK *b)
+{
+
+    printf("#---------------------------------------------------------#\n");
+    FILE *catptr;
+    CAT c;
+    catptr = fopen("../BooksDB/Categories.txt", "r+");
+    printf("--EXISTING CATEGORIES--\n");
+    while (fread(&c, sizeof(c), 1, catptr))
+    {
+        printf("[%d] %s\n", c.no, c.name);
+    }
+    printf("\n--EXISTING CATEGORIES--\n");
+    fclose(catptr);
+    char yn[1];
+    while (1)
+    {
+        printf("\nDo you wish to ADD a NEW CATEGORY to the above list? [y/n]: ");
+        scanf("%s", yn);
+        if (tolower(yn[0]) == 'y')
+        {
+            getchar();
+            add_categories();
+            break;
+        }
+        else if (tolower(yn[0]) == 'n')
+        {
+            break;
+        }
+        else
+            printf("Invalid Input.\n");
+    }
+    if (tolower(yn[0]) == 'y')
+    {
+        catptr = fopen("../BooksDB/Categories.txt", "r+");
+        printf("\n--EXISTING CATEGORIES--\n");
+        while (fread(&c, sizeof(c), 1, catptr))
+        {
+            printf("[%d] %s\n", c.no, c.name);
+        }
+        printf("\n--EXISTING CATEGORIES--\n");
+        fclose(catptr);
+    }
+    printf("\nAssign Category to the book by the corresponding SERIAL NUMBER. \nENTER 0 to STOP assigning.\n");
+
+    while (1)
+    {
+        int counter = 0;
+        int sno[50];
+        while (1)
+        {
+
+            printf("  Assign %dth category of the book(by serial number): ", (counter + 1));
+            scanf("%d", &sno[counter]);
+            if (sno[counter] == 0)
+                break;
+            else
+                counter++;
+        }
+        counter = 0;
+        printf("\nEntered Categories: ");
+        while (sno[counter] != 0)
+        {
+            printf("%d ", sno[counter]);
+            counter++;
+        }
+        counter = 0;
+        printf("\nConfirm Book Categories? [y/n]: ");
+        scanf("%s", yn);
+        if (tolower(yn[0]) == 'y')
+        {
+            while (sno[counter] != 0)
+            {
+                b->categories[counter] = sno[counter];
+                counter++;
+            }
+            printf("Book Categories Confirmed.\n");
+            break;
+        }
+        else if (tolower(yn[0]) == 'n')
+        {
+            printf("Re-enter[1] Exit[0]: ");
+            scanf("%d", &counter);
+            if (counter == 0)
+                break;
+            else
+                printf("Re-ENTRY\n");
+        }
+    }
+    printf("#----------------------------------------------------------#\n\n");
+}
+
 int *generate_uid(BOOK *b)
 {
     FILE *uniq;
     uniq = fopen("../BooksDB/UID.txt", "r+");
     int n, i, j;
-    fread(&n, sizeof(int), 1, uniq);
-
+    n = 0;
+    if (!fread(&n, sizeof(int), 1, uniq))
+    {
+        printf("First Book Entered\n.");
+        fwrite(&n, sizeof(int), 1, uniq);
+    };
+    printf("%d\n", n);
     for (i = 0; i < 50; i++)
     {
         if (b->available_UID[i] == 0)
@@ -73,14 +221,23 @@ int add_book()
         fgets(b1.publisher, 50, stdin);
         b1.publisher[strcspn(b1.publisher, "\n")] = '\0';
 
+        input_categories(&b1);
         printf("Number of Copies: ");
-        scanf("%d", &b1.available_copies);
+        if (!scanf("%d", &b1.available_copies))
+        {
+            printf("Invalid Input.\n");
+            return 0;
+        }
 
         while (1)
         {
-            printf("\nConfirm Input[1] , Re-enter[2] or back-to-menu[3]?: ");
+            printf("\nConfirm Book Entry [1] , Re-enter[2] or back-to-menu[3]?: ");
             int cnfm;
-            scanf("%d", &cnfm);
+            if (!scanf("%d", &cnfm))
+            {
+                printf("Invalid Input.\n");
+                return 0;
+            }
             if (cnfm == 3)
                 return 0;
             else if (cnfm == 2)
