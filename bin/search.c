44d9a5b9b0d6c695ec49a8f881f941fc3d164da8
perplexedkitty <ahannandi2003@gmail.com>
2023-11-16 23:26:11 +0530

    Modified search

--- bin/search.c
+++ bin/search.c
@@ -2,44 +2,64 @@
 #include <string.h>
 #include <stdlib.h>
 #include "structs.h"
-//#include "member_menu.c"
+// #include "member_menu.c"
+
+void dataSearch(char str[100])
+{
+    printf("Here!");
+}
 
 int search()
 {
-    extern int *usertype;
     system("clear");
     printf("\nYou have chosen to search for a book.\n\n");
     printf("Enter '1' to search by name of the book\n");
     printf("Enter '2' to search by name of the author(s)\n");
     printf("Enter '3' to search by ISBN code of the book\n");
     printf("Enter '4' to go back to menu\n");
-    printf("Enter your choice: ");
+    
     int choice;
     while (1)
     {
+        printf("Enter your choice: ");
         scanf("%d", &choice);
+        getchar();
         switch (choice)
         {
         case 1:
-            printf("Case 1!!!");
-            break;
+            char name_of_book[100];
+            printf("\nEnter the name of the book: ");
+            fgets(name_of_book, 100, stdin);
+            name_of_book[strcspn(name_of_book, "\n")] = '\0';
+            dataSearch(name_of_book);
+            return 1;
+
         case 2:
-            printf("Case 2!!!");
-            break;
+            char name_of_auth[100];
+            printf("\nEnter the name of the author: ");
+            fgets(name_of_auth, 50, stdin);
+            name_of_auth[strcspn(name_of_auth, "\n")] = '\0';
+            dataSearch(name_of_auth);
+            return 1;
+
         case 3:
-            printf("Case 3!!!");
-            break;
+            char isbn[100];
+
+            printf("\nEnter the ISBN code of the book: ");
+            fgets(isbn, 14, stdin);
+            isbn[strcmp(isbn, "\n")] = '\0';
+            dataSearch(isbn);
+            return 1;
+
         case 4:
             return 0;
         default:
             system("clear");
             printf("Wrong choice! Enter again...");
-            printf("\nYou have chosen to search for a book.\n\n");
             printf("Enter '1' to search by name of the book\n");
             printf("Enter '2' to search by name of the author(s)\n");
             printf("Enter '3' to search by ISBN code of the book\n\n");
             printf("Enter '4' to go back to menu\n");
-            printf("Enter your choice: ");
         }
     }
 }
